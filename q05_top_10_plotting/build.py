9b3355f67737447a9ac64eeeecfc3ce3e1541eb2
Nikhil Nair <nnair25@gmail.com>
2018-07-18 16:20:10 +0530

    Updated readme. Added import to build

--- q05_top_10_plotting/build.py
+++ q05_top_10_plotting/build.py
@@ -1,6 +1,7 @@
 # default imports
 import matplotlib.pyplot as plt
 from greyatomlib.olympics_project_new.q04_find_top_10.build import q04_find_top_10, q03_better_event, q02_country_operations, q01_rename_columns
+plt.switch_backend('agg')
 path = "./data/olympics.csv"
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
