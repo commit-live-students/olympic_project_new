0af4867edf5b5ea487af2991967573e088ad5de7
amagarwal-18 <amitagarwal.skit@gmail.com>
2018-08-04 09:53:18 +0000

    Done

--- q02_country_operations/build.py
+++ q02_country_operations/build.py
@@ -1,11 +1,20 @@
+# %load q02_country_operations/build.py
 # default imports
+import pandas as pd
 from greyatomlib.olympics_project_new.q01_rename_columns.build import q01_rename_columns
 #Previous Functions
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 
+def q02_country_operations(df):
+    df['Country Name'] = df['Country'][:]
+    df['Country name'], df['Code'] = (df['Country Name'].str.split('(',1).str)
+    df.drop(df['Code'], axis = 0, inplace = True)
+    df[100,16] = 'Portugal'
+    return df
 
-
+q02_country_operations(OlympicsDF).iloc[100,16]
+q02_country_operations(OlympicsDF)
 
 
 
