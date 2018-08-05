7cc87517d2c6543e7d480552b8ea32710b67e906
sonalverma1 <sonalverma1@gmail.com>
2018-08-05 05:45:52 +0000

    Done

--- q02_country_operations/build.py
+++ q02_country_operations/build.py
@@ -5,12 +5,14 @@ from greyatomlib.olympics_project_new.q01_rename_columns.build import q01_rename
 path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)
 def q02_country_operations(dataframe):    
-    OlympicsDF['Country Name'] = OlympicsDF['Country'].str.split('(').str[0]
+    OlympicsDF['Country Name'] = OlympicsDF['Country'].str.split('(').str[0].str.strip()
     OlympicsDF['Country Name']
     return OlympicsDF
+   # return OlympicsDF['Country'].str.split('(').str[0].str.strip()
 
 a = q02_country_operations(OlympicsDF)
 
+a
 
 a[a['Country Name']=='Portugal']
 OlympicsDF['Country Name'] = OlympicsDF['Country'].str.split('(').str[0].str.rstrip('\W')
