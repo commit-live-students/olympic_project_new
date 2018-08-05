92ae16a37b87c4a24fd12ea77e24db9852e97b9c
sahusaurabh65 <sahusaurabh65@gmail.com>
2018-08-05 05:05:20 +0000

    Done

--- q02_country_operations/build.py
+++ q02_country_operations/build.py
@@ -3,25 +3,32 @@
 from greyatomlib.olympics_project_new.q01_rename_columns.build import q01_rename_columns
 #Previous Functions
 import pandas as pd
+import re
+re.compile('<title>(.*)</title>')
 path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 
-def q02_country_operations(df):
-    df['Country Name']=df['Country'][:] 
-    df['Country name']=(df['Country Name'].str.split('(',1).str)[0].str.strip
+# def q02_country_operations(df):
+#     df['Country Name']=df['Country'][:] 
+#     df['Country name']=(df['Country Name'].str.split('(',1).str)[0].str.strip
     
-    return df
+#     return df
+# # q02_country_operations(OlympicsDF).iloc[100,16]
 # q02_country_operations(OlympicsDF).iloc[100,16]
-q02_country_operations(OlympicsDF).iloc[100,16]
+# #print(OlympicsDF.iloc[100,16])
+def q02_country_operations(OlympicsDF):
+    country_name = []
+    for country  in OlympicsDF['Country']:
+        position = re.search('\(',country)
+        if position!=None:
+            country_name.append(country[:position.end()-2])
+        else:
+            country_name.append(country)
+    OlympicsDF['Country Name'] = pd.Series(country_name)
+    return OlympicsDF
 
 
-
-#print(OlympicsDF.iloc[100,16])
-
-
-
-
-
+q02_country_operations(OlympicsDF)
 
 
 
