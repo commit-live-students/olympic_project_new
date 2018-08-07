d1d8d31a23119b99e73f78bbb209e983d22034a3
sonalverma1 <sonalverma1@gmail.com>
2018-08-07 06:29:31 +0000

    Done

--- q05_top_10_plotting/build.py
+++ q05_top_10_plotting/build.py
@@ -1,15 +1,25 @@
+# %load q05_top_10_plotting/build.py
 # default imports
 import matplotlib.pyplot as plt
 from greyatomlib.olympics_project_new.q04_find_top_10.build import q04_find_top_10, q03_better_event, q02_country_operations, q01_rename_columns
 plt.switch_backend('agg')
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
 OlympicsDF=q03_better_event(OlympicsDF)    
 Top10Summer,Top10Winter, Top10, Common =q04_find_top_10(OlympicsDF,'Total_Summer', 'Total_Winter','Total')
+def q05_top_10_plotting(OlympicsDF,Top10Summer,Top10Winter,Top10):
+    a = OlympicsDF[OlympicsDF['Country_Name'].isin(Top10Summer)]['Total']
+    plt.plot(Top10Summer,a)
+    plt.show()
+    b = OlympicsDF[OlympicsDF['Country_Name'].isin(Top10Winter)]['Total']
+    plt.plot(Top10Winter,b)
+    plt.show()
+    c = OlympicsDF[OlympicsDF['Country_Name'].isin(Top10)]['Total']
+    plt.plot(Top10Summer,c)
+    plt.show()
 
-
-
+q05_top_10_plotting(OlympicsDF,Top10Summer,Top10Winter,Top10)
 
 
 
