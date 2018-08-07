99828d958c945001e4b00072049e548336635939
kaushik1975 <kaushik1975@gmail.com>
2018-08-07 06:45:13 +0000

    Done

--- q02_country_operations/build.py
+++ q02_country_operations/build.py
@@ -6,8 +6,16 @@ path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 
 
+def SubString(str1):
+    str2 = '('
+    CharFindPosition = str1.find(str2)
+    return_String = str1[:CharFindPosition]
+    return return_String.strip()
+
 def q02_country_operations(OlympicsDF):
-    df = (OlympicsDF['Country'].str.extract('([A-Z]\w{0,})', expand=True))
+    #df = (OlympicsDF['Country'].str.extract('([A-Z]\w{0,})', expand=True))
+    df = OlympicsDF['Country']
+    df = df.apply(SubString)
     OlympicsDF['Country_Name'] = df
     return OlympicsDF
 
