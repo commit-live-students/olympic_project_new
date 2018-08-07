7e33f6f61bba49ddc7abec5f4b3e692fdb495f50
kaushik1975 <kaushik1975@gmail.com>
2018-08-07 14:00:43 +0000

    Done

--- q05_top_10_plotting/build.py
+++ q05_top_10_plotting/build.py
@@ -1,12 +1,37 @@
+# %load q05_top_10_plotting/build.py
 # default imports
 import matplotlib.pyplot as plt
 from greyatomlib.olympics_project_new.q04_find_top_10.build import q04_find_top_10, q03_better_event, q02_country_operations, q01_rename_columns
 plt.switch_backend('agg')
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
 OlympicsDF=q03_better_event(OlympicsDF)    
 Top10Summer,Top10Winter, Top10, Common =q04_find_top_10(OlympicsDF,'Total_Summer', 'Total_Winter','Total')
+
+
+def q05_top_10_plotting(df, Top10Summer, Top10Winter, Top10):
+    df_Summer = df[df['Country_Name'].isin(Top10Summer)]
+    df_Winter = df[df['Country_Name'].isin(Top10Winter)]
+    df_Total  = df[df['Country_Name'].isin(Top10)]
+
+    plt.bar(df_Summer['Country_Name'],df_Summer['Total_Summer'])
+    plt.title = 'Top 10 Summer'
+    plt.xlabel = 'Country Name'
+    plt.ylabel = 'Total Medals'
+    plt.show()
+
+    plt.bar(df_Winter['Country_Name'],df_Summer['Total_Winter'])
+    plt.title = 'Top 10 Winter'
+    plt.xlabel = 'Country Name'
+    plt.ylabel = 'Total Medals'
+    plt.show()
+
+    plt.bar(df_Total['Country_Name'],df_Summer['Total'])
+    plt.title = 'Top 10'
+    plt.xlabel = 'Country Name'
+    plt.ylabel = 'Total Medals'
+    plt.show()
 
 
 
--- q07_unusual_performances/build.py
+++ q07_unusual_performances/build.py
@@ -1,5 +1,16 @@
+# %load q07_unusual_performances/build.py
 # default imports
 from greyatomlib.olympics_project_new.q02_country_operations.build import q02_country_operations, q01_rename_columns
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
+
+def q07_unusual_performances(df, low, high):
+    df.drop(df.tail(1).index,inplace=True)
+    quant_df = OlympicsDF['Total'].quantile([low, high])
+    DFHigh = df.loc[df['Total']>=quant_df.loc[high],['Country_Name']]
+    DFLow = df.loc[df['Total']<=quant_df.loc[low], ['Country_Name']]
+    return DFLow, DFHigh
+
+
+
