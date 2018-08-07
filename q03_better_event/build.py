3924f7a629743e5d5a155e95e5dca8032d5d4d0f
Tusharsharma86 <tusharsharma86@gmail.com>
2018-08-07 07:18:47 +0000

    Done

--- q03_better_event/build.py
+++ q03_better_event/build.py
@@ -1,13 +1,36 @@
+# %load q03_better_event/build.py
 #default imports
 from greyatomlib.olympics_project_new.q02_country_operations.build import q02_country_operations, q01_rename_columns
 import numpy as np
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+
 #Previous function
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
 
+#Function definition
+def q03_better_event(OlympicsDF):
+    i=0
+    #Create a new blank column
+    OlympicsDF['BetterEvent'] = pd.Series([])
 
-
-
+    #Initiate a loop to compare each row value in totl winter summer and then set value 
+    while i < OlympicsDF['Total_Summer'].size:
+        if OlympicsDF['Total_Summer'][i] > OlympicsDF['Total_Winter'][i]:
+            OlympicsDF['BetterEvent'][i] = 'Summer'
+        elif OlympicsDF['Total_Summer'][i] < OlympicsDF['Total_Winter'][i]:
+            OlympicsDF['BetterEvent'][i] = 'Winter'
+        else:
+            OlympicsDF['BetterEvent'][i] = 'Both'
+        i=i+1
+    return OlympicsDF
+    
+#Function call    
+q03_better_event(OlympicsDF)
+OlympicsDF['BetterEvent'].value_counts().max()
+OlympicsDF.shape
+OlympicsDF
 
 
