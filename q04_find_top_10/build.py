caba60d4764d3a463305f9f1e9f06895853cf112
Akash Desarda <desardaakash@gmail.com>
2018-08-08 22:21:06 +0530

    Update build.py

--- q04_find_top_10/build.py
+++ q04_find_top_10/build.py
@@ -5,18 +5,18 @@ path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
 OlympicsDF=q03_better_event(OlympicsDF)
-#OlympicsDF=OlympicsDF.drop(OlympicsDF.index[146])
+OlympicsDF=OlympicsDF.drop(OlympicsDF.index[146])
 def q04_find_top_10(df,Total_Summer,Total_Winter,Total):
     summer,winter,total,common=[],[],[],[]
-    #x=df.nlargest(10,'Total_Summer')
-    summer=['United States','Soviet Union','Great Britain','France','Germany','Italy','Sweden','Hungary','China','Australia']
-   # y=df.nlargest(10,'Total_Winter')
-    winter=['Norway','United States','Austria','Germany','Soviet Union','Canada','Finland','Sweden','Switzerland','Russia']
-    #z=df.nlargest(10,'Total')
-    total=['United States','Soviet Union','Great Britain','Germany','France','Italy','Sweden','China','East Germany','Russia']
-    common=['Germany', 'Sweden', 'United States', 'Soviet Union']
+    x=df.nlargest(10,'Total_Summer')
+    summer=x['Country_Name'].tolist()
+    y=df.nlargest(10,'Total_Winter')
+    winter=y['Country_Name'].tolist() 
+    z=df.nlargest(10,'Total')
+    total=z['Country_Name'].tolist()
+    common=list(set(summer).intersection(set(winter).intersection(set(total))))
+    common.sort()
     return summer,winter,total,common
-q04_find_top_10(OlympicsDF,'Total_Summer','Total_Winter','Total')
-OlympicsDF.tail()
+q04_find_top_10(OlympicsDF,"Total_Summer","Total_Winter","Total")
 
 
