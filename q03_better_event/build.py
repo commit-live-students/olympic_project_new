af244c9cd1ce460de643f7a16a79ca4303c952ad
ankit-a-mehta <ankitmehta195@gmail.com>
2018-08-09 06:44:41 +0000

    Done

--- q03_better_event/build.py
+++ q03_better_event/build.py
@@ -1,13 +1,25 @@
+# %load q03_better_event/build.py
 #default imports
 from greyatomlib.olympics_project_new.q02_country_operations.build import q02_country_operations, q01_rename_columns
 import numpy as np
 #Previous function
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
 
+def filterSumWinBoth(row):
+    if (row['Total_Summer'] > row['Total_Winter']):
+        return'Summer'
+    elif (row['Total_Summer'] < row['Total_Winter']):
+        return 'Winter'
+    else:
+        return 'Both'
 
+def q03_better_event(OlympicsDF):
+    OlympicsDF['BetterEvent'] = OlympicsDF.apply(lambda x: filterSumWinBoth(x), axis = 1)
+    return OlympicsDF
 
+q03_better_event(OlympicsDF)
 
 
 
