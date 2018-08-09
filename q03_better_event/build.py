67cc6e07bc61b5be66975e206d173299a683eebb
krupa25 <shahkrupa25@gmail.com>
2018-08-09 05:43:27 +0000

    Done

--- q03_better_event/build.py
+++ q03_better_event/build.py
@@ -1,13 +1,17 @@
+# %load q03_better_event/build.py
 #default imports
 from greyatomlib.olympics_project_new.q02_country_operations.build import q02_country_operations, q01_rename_columns
 import numpy as np
+import pandas as pd
 #Previous function
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
 
 
-
-
-
+def q03_better_event(q02_country_operations):
+    df = OlympicsDF
+    df['BetterEvent'] = np.where((df['Total_Summer']>df['Total_Winter']),'Summer',np.where(df['Total_Summer']<df['Total_Winter'],'Winter','Both'))
+    return df
+q03_better_event(q02_country_operations)
 
