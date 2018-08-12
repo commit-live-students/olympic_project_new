68a56042d50b9b9cf3dc31b70c2c382535b0fff0
ramyakrishnan86 <ramya_krishnan86@yahoo.co.in>
2018-08-12 15:29:17 +0000

    Done

--- q05_top_10_plotting/build.py
+++ q05_top_10_plotting/build.py
@@ -1,15 +1,20 @@
+# %load q05_top_10_plotting/build.py
 # default imports
 import matplotlib.pyplot as plt
 from greyatomlib.olympics_project_new.q04_find_top_10.build import q04_find_top_10, q03_better_event, q02_country_operations, q01_rename_columns
 plt.switch_backend('agg')
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
 OlympicsDF=q03_better_event(OlympicsDF)    
 Top10Summer,Top10Winter, Top10, Common =q04_find_top_10(OlympicsDF,'Total_Summer', 'Total_Winter','Total')
 
-
-
-
-
+def q05_top_10_plotting(OlympicsDF,Top10,Top10Summer,Top10Winter):
+    OlympicsDF2 = OlympicsDF.iloc[:len(OlympicsDF)-1,:]
+    top_ten_winter = OlympicsDF2.nlargest(10,'Total_Winter')[['Country_Name','Total_Winter']]
+    top_ten_summer = OlympicsDF2.nlargest(10,'Total_Summer')[['Country_Name','Total_Summer']]
+    top_ten_total = OlympicsDF2.nlargest(10,'Total')[['Country_Name','Total']]
+    plt.bar(top_ten_total['Country_Name'],top_ten_summer['Total_Summer'])
+    plt.bar(top_ten_total['Country_Name'],top_ten_winter['Total_Winter'])
+    plt.bar(top_ten_total['Country_Name'],top_ten_total['Total'])
 
