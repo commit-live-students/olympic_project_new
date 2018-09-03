680594de23e09420659017e9413e0089ff05f461
NIkhila79 <nikhila.dalvi@gmail.com>
2018-09-03 14:38:49 +0000

    Done

--- q02_country_operations/build.py
+++ q02_country_operations/build.py
@@ -1,11 +1,14 @@
+# %load q02_country_operations/build.py
 # default imports
 from greyatomlib.olympics_project_new.q01_rename_columns.build import q01_rename_columns
 #Previous Functions
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
-
-
-
+def q02_country_operations(OlympicsDF=OlympicsDF):
+    newcolumlist = [ (str(x)).split('(',1)[0].replace(u'\xa0', u'')   for x in  OlympicsDF['Country']]
+    OlympicsDF['Country_Name']=newcolumlist
+    return OlympicsDF
+q02_country_operations()
 
 
 
