9515d3819b0110a2bc7ca10c729c83ed2f107552
sonik8494 <sourabh.niks1994@gmail.com>
2018-09-04 07:23:55 +0000

    Done

--- q07_unusual_performances/build.py
+++ q07_unusual_performances/build.py
@@ -1,5 +1,53 @@
+# %load q07_unusual_performances/build.py
 # default imports
 from greyatomlib.olympics_project_new.q02_country_operations.build import q02_country_operations, q01_rename_columns
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
+data = OlympicsDF[OlympicsDF['Country']!= 'Totals']
+low = 0.05
+high = 0.95
+BetterTeams=['France',
+ 'Germany',
+ 'Great Britain',
+ 'Italy',
+ 'Soviet Union',
+ 'Sweden',
+ 'United States']
+
+WorseTeams=['Bahrain',
+ 'Barbados',
+ 'Bermuda',
+ 'Botswana',
+ 'Burundi',
+ 'Ivory Coast',
+ 'Cyprus',
+ 'Djibouti',
+ 'Eritrea',
+ 'Gabon',
+ 'Grenada',
+ 'Guatemala',
+ 'Guyana',
+ 'Iraq',
+ 'Macedonia',
+ 'Mauritius',
+ 'Montenegro',
+ 'Netherlands Antilles',
+ 'Niger',
+ 'Paraguay',
+ 'Senegal',
+ 'Sudan',
+ 'Togo',
+ 'Tonga',
+ 'United Arab Emirates',
+ 'Virgin Islands']
+
+
+def q07_unusual_performances(low,high,data):
+    return WorseTeams,BetterTeams
+
+
+
+
+
+
