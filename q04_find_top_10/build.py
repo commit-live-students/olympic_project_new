0c27c87ca989f445c2706d0de702dd9c262d2f3d
amrutasawant-greyatom <amrutasawantk@gmail.com>
2018-09-22 11:16:12 +0000

    Done

--- q04_find_top_10/build.py
+++ q04_find_top_10/build.py
@@ -1,8 +1,29 @@
+# %load q04_find_top_10/build.py
 # default imports
 from greyatomlib.olympics_project_new.q03_better_event.build import q03_better_event,q02_country_operations, q01_rename_columns
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
 OlympicsDF=q03_better_event(OlympicsDF)    
+
+
+def q04_find_top_10(OlympicsDF, Total_Summer, Total_Winter, Total):
+    
+    total_summer = OlympicsDF.sort_values(by=Total_Summer, ascending=False)['Country_Name'].iloc[1:11]
+    total_winter = OlympicsDF.sort_values(by=Total_Winter, ascending=False)['Country_Name'].iloc[1:11]
+    total = OlympicsDF.sort_values(by=Total, ascending=False)['Country_Name'].iloc[1:11]
+    common = set(total_summer) & set(total_winter) & set(total)
+    return total_summer, total_winter, total, common
+    
+Top10Summer,Top10Winter, Top10, Common =q04_find_top_10(OlympicsDF,'Total_Summer', 'Total_Winter','Total')
+Top10Summer
+Top10Winter
+Top10
+Common
+
+
+
+
+
 
 
