32c3f17f98b72988134b1c31c4c74740523bfea5
amrutasawant-greyatom <amrutasawantk@gmail.com>
2018-09-22 05:14:22 +0000

    Done

--- q03_better_event/build.py
+++ q03_better_event/build.py
@@ -1,13 +1,33 @@
+# %load q03_better_event/build.py
 #default imports
 from greyatomlib.olympics_project_new.q02_country_operations.build import q02_country_operations, q01_rename_columns
 import numpy as np
 #Previous function
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
 
+def q03_better_event(OlympicsDF):
+    
+    OlympicsDF['BetterEvent'] = np.empty
+    for i in range(len(OlympicsDF)):
+        if OlympicsDF.iloc[i, 5] > OlympicsDF.iloc[i, 10]:
+            #summer 
+            OlympicsDF['BetterEvent'][i] = 'Summer'
+        if OlympicsDF.iloc[i, 5] < OlympicsDF.iloc[i, 10]:
+            #winter
+            OlympicsDF['BetterEvent'][i] = 'Winter'
+        if OlympicsDF.iloc[i, 5] == OlympicsDF.iloc[i, 10]:
+            #both
+            OlympicsDF['BetterEvent'][i] = 'Both'
+
+    return OlympicsDF
 
 
+df = q03_better_event(OlympicsDF)
+df.BetterEvent.value_counts().max()
+df.shape
+df.BetterEvent.value_counts().max()
 
 
 
