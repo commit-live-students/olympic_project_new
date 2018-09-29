fb4481dee50dfe23a8bbe1b020372dc18107b0ae
sunilhariharan <sunilhariharan@gmail.com>
2018-09-29 10:33:57 +0000

    Done

--- q01_rename_columns/build.py
+++ q01_rename_columns/build.py
@@ -1,5 +1,20 @@
+# %load q01_rename_columns/build.py
 # default imports
 import pandas as pd
 
+path='./data/olympics.csv'
+
+def q01_rename_columns(path):
+    
+    df=pd.read_csv(path,skiprows=[0])
+    df.rename(columns={'Combined total':'Total','Total.1':'Total_Winter','Total':'Total_Summer'},inplace=True)
+    df.columns.values[0] = 'Country'
+    df.columns = df.columns.str.replace('01 !', 'Gold_Summer')
+    df.columns = df.columns.str.replace('02 !', 'Silver_Summer')
+    df.columns = df.columns.str.replace('03 !', 'Bronze_Summer')
+    #df.columns = df.columns.str.replace('!', '  _Summer')   
+    df.columns = df.columns.str.replace('Summer.1', 'Winter') 
+    df.columns = df.columns.str.replace('Summer.2', 'Total')
+    return df
 
 
