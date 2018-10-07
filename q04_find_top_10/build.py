14bc3cdde8f7660c83618f051146a4fc177e6c8d
sreelekhaWoonna <sreelekha4877@gmail.com>
2018-10-07 10:00:48 +0000

    Done

--- q04_find_top_10/build.py
+++ q04_find_top_10/build.py
@@ -1,8 +1,24 @@
+# %load q04_find_top_10/build.py
 # default imports
+import pandas as pd
 from greyatomlib.olympics_project_new.q03_better_event.build import q03_better_event,q02_country_operations, q01_rename_columns
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
 OlympicsDF=q03_better_event(OlympicsDF)    
+
+def q04_find_top_10(df, col1, col2, col3):
+    df = df[df.Country_Name != 'Totals']
+    summer = df.nlargest(10, col1)['Country_Name']
+    winter = df.nlargest(10, col2)['Country_Name']
+    both = df.nlargest(10, col3)['Country_Name']
+    intersection = pd.Series(list(set(summer).intersection(set(winter))))
+    return summer,winter,both,pd.Series(list(set(intersection).intersection(set(both))))
+
+s1,s2,s3,s4 = q04_find_top_10(OlympicsDF,'Total_Summer', 'Total_Winter','Total')
+s1
+s2
+s3
+
 
 
--- q05_top_10_plotting/build.py
+++ q05_top_10_plotting/build.py
@@ -1,12 +1,20 @@
+# %load q05_top_10_plotting/build.py
 # default imports
 import matplotlib.pyplot as plt
 from greyatomlib.olympics_project_new.q04_find_top_10.build import q04_find_top_10, q03_better_event, q02_country_operations, q01_rename_columns
 plt.switch_backend('agg')
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
 OlympicsDF=q03_better_event(OlympicsDF)    
 Top10Summer,Top10Winter, Top10, Common =q04_find_top_10(OlympicsDF,'Total_Summer', 'Total_Winter','Total')
+def q05_top_10_plotting(OlympicsDF,Top10Summer,Top10Winter, Top10):
+    Summer = OlympicsDF[OlympicsDF['Country_Name'].isin(Top10Summer)]
+    Winter = OlympicsDF[OlympicsDF['Country_Name'].isin(Top10Winter)]
+    Both = OlympicsDF[OlympicsDF['Country_Name'].isin(Top10)]
+    Summer.plot(x='Country_Name', y='Total', kind='bar')
+    Winter.plot(x='Country_Name', y='Total', kind='bar')
+    Both.plot(x='Country_Name', y='Total', kind='bar')
 
 
 
