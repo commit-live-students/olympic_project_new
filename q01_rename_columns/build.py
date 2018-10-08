298d08e553d478ba862d206cb9c0e8fc373973bd
tracedence <shoaibrashid887@gmail.com>
2018-10-08 11:28:43 +0000

    Done

--- q01_rename_columns/build.py
+++ q01_rename_columns/build.py
@@ -1,5 +1,19 @@
+# %load q01_rename_columns/build.py
 # default imports
 import pandas as pd
+
+path = './data/olympics.csv'
+
+def q01_rename_columns(path=path):
+    df = pd.read_csv(path, skiprows=1)
+    #df.rename(index={0:'a'},inplace=True)
+    df.rename(columns={'Unnamed: 0': 'Country','01 !':'Gold_Summer','02 !':'Silver_Summer','03 !':'Bronze_Summer',
+                  '01 !.1':'Gold_Winter','02 !.1':'Silver_Winter','03 !.1':'Bronze_Winter',
+                  '01 !.2':'Gold_Total','02 !.2':'Silver_Total','03 !.2':'Bronze_Total',
+                  'Total':'Total_Summer','Total.1':'Total_Winter','Combined total': 'Total'},inplace = True)
+
+    return df
+q01_rename_columns(path)
 
 
 
