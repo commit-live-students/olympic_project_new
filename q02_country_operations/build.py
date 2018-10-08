45a9d0c932a1c27dad995c05331024d5bb176b72
tracedence <shoaibrashid887@gmail.com>
2018-10-08 11:55:21 +0000

    Done

--- q02_country_operations/build.py
+++ q02_country_operations/build.py
@@ -1,11 +1,24 @@
+# %load q02_country_operations/build.py
 # default imports
+import pandas as pd
 from greyatomlib.olympics_project_new.q01_rename_columns.build import q01_rename_columns
 #Previous Functions
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 
+def q02_country_operations(data):
+    
+    testing_col = data['Country']
+    temp_result = []
+    for value in testing_col:
+        temp_result.append(value.split()[0])
+
+    data['Country_Name'] = pd.Series(temp_result)
+    
+    return data
 
 
+q02_country_operations(OlympicsDF)
 
 
 
