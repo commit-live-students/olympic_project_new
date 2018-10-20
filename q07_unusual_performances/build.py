9c444a62ae8cab75d5e20c3fcbb39851f8647a8b
ankit-a-mehta <ankitmehta195@gmail.com>
2018-10-20 18:09:17 +0000

    Done

--- q07_unusual_performances/build.py
+++ q07_unusual_performances/build.py
@@ -10,15 +10,23 @@ import numpy as np
 
 low = 0.05
 high = 0.95
-
 def q07_unusual_performances(OlympicsDF, low, high):
-    low_value, high_value = OlympicsDF['Total'].quantile([low, high])
+    OlympicsDF = OlympicsDF.drop(OlympicsDF.index[23])
+    
+    low_value, high_value = OlympicsDF['Total'].quantile([0.05, 0.95])
 
     low_dataframe = OlympicsDF.iloc[np.where(OlympicsDF['Total'] <= low_value)]
     low_countries= low_dataframe['Country_Name']
+    low_countries = low_countries.tolist()
+    if('Totals' in low_countries):
+        high_countries.remove('Totals')
     
     high_dataframe = OlympicsDF.iloc[np.where(OlympicsDF['Total'] > high_value)]
-    high_countries = high_dataframe['Country_Name'][1:]
+    high_countries = high_dataframe['Country_Name']
+    
+    high_countries = high_countries.tolist()
+    if('Totals' in high_countries):
+        high_countries.remove('Totals')
     
     return low_countries, high_countries
 
