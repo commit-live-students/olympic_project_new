f263566e72aa0a74b8582f8bb5b5f77d21733ca0
simpleguyon <bhandaru.dheeraj@gmail.com>
2018-11-20 18:09:42 +0000

    Done

--- q03_better_event/build.py
+++ q03_better_event/build.py
@@ -1,13 +1,26 @@
+# %load q03_better_event/build.py
 #default imports
 from greyatomlib.olympics_project_new.q02_country_operations.build import q02_country_operations, q01_rename_columns
 import numpy as np
 #Previous function
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
 
 
+def q03_better_event(OlympicsDF):
+    OlympicsDF['BetterEvent'] = OlympicsDF.apply(f, axis=1)
+    return OlympicsDF 
 
 
 
+
+q03_better_event(OlympicsDF)
+
+
+
+
+
+
+|
 
