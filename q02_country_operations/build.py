731a17554d9d59a98741f6a9bfbdd547fdbf86f2
simpleguyon <bhandaru.dheeraj@gmail.com>
2018-11-20 18:06:46 +0000

    Done

--- q02_country_operations/build.py
+++ q02_country_operations/build.py
@@ -1,8 +1,20 @@
+# %load q02_country_operations/build.py
 # default imports
 from greyatomlib.olympics_project_new.q01_rename_columns.build import q01_rename_columns
 #Previous Functions
-path = "./data/olympics.csv"
-OlympicsDF=q01_rename_columns(path)    
+path = './data/olympics.csv'
+OlympicsDF=q01_rename_columns(path)  
+
+def q02_country_operations(OlympicsDF):
+    OlympicsDF['Country names']=OlympicsDF['Country'].apply(lambda x : (x.split('(')[0]).replace(u'\xa0',u''))
+    return OlympicsDF
+
+
+q02_country_operations(OlympicsDF)
+
+
+
+
 
 
 
