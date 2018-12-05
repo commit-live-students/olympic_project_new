ac54236c6529df6d6ffaf3fc2f74cac8f8a5a679
nehatanwani <neha29616@gmail.com>
2018-12-05 05:02:15 +0000

    Done

--- q02_country_operations/build.py
+++ q02_country_operations/build.py
@@ -1,8 +1,15 @@
-# default imports
-from greyatomlib.olympics_project_new.q01_rename_columns.build import q01_rename_columns
-#Previous Functions
-path = "./data/olympics.csv"
+#default imports
+from greyatomlib.olympics_project_new.q02_country_operations.build import q02_country_operations, q01_rename_columns
+import numpy as np
+#Previous function
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
+OlympicsDF=q02_country_operations(OlympicsDF)
+
+
+
+
+
 
 
 
