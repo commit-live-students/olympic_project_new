1456ef673120fe9caa8bc2ebc78a5d3730f5f6bd
vivekshingate <vivekshingate@ymail.com>
2018-12-06 13:51:18 +0000

    Done

--- q05_top_10_plotting/build.py
+++ q05_top_10_plotting/build.py
@@ -1,15 +1,34 @@
+# %load q05_top_10_plotting/build.py
 # default imports
 import matplotlib.pyplot as plt
+import pandas as pd
 from greyatomlib.olympics_project_new.q04_find_top_10.build import q04_find_top_10, q03_better_event, q02_country_operations, q01_rename_columns
 plt.switch_backend('agg')
-path = "./data/olympics.csv"
+path = './data/olympics.csv'
 OlympicsDF=q01_rename_columns(path)    
 OlympicsDF=q02_country_operations(OlympicsDF)
 OlympicsDF=q03_better_event(OlympicsDF)    
-Top10Summer,Top10Winter, Top10, Common =q04_find_top_10(OlympicsDF,'Total_Summer', 'Total_Winter','Total')
+Top10Summer,Top10Winter, Top10, Common = q04_find_top_10(OlympicsDF,'Total_Summer', 'Total_Winter','Total')
 
 
+def q05_top_10_plotting(Dataset,col1=Top10Summer,col2=Top10Winter,col3=Top10):
 
+    OlympicsDF[OlympicsDF['Country_Name'].isin(col1)].plot(x='Country_Name',y='Total_Summer',kind='bar')
+    plt.title('Top 10 Summer')
+    plt.xlabel('Country Name')
+    plt.ylabel('Total Medals')
 
+    OlympicsDF[OlympicsDF['Country_Name'].isin(col2)].plot(x='Country_Name',y='Total_Winter',kind='bar')
+    plt.title('Top 10 Winter')
+    plt.xlabel('Country Name')
+    plt.ylabel('Total Medals')
 
+    OlympicsDF[OlympicsDF['Country_Name'].isin(col3)].plot(x='Country_Name',y='Total',kind='bar')
+    plt.title('Top 10')
+    plt.xlabel('Country Name')
+    plt.ylabel('Total Medals')
+    plt.show() 
+
+#Call to the function
+q05_top_10_plotting(OlympicsDF,Top10Summer,Top10Winter,Top10)
 
